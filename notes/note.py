7ea42edbc62302c945b0bf07ae2cc47308f89dfe
Lucas Wiman <lucas.wiman@akasa.com>
2022-04-25 11:37:12 -0700

    Move to simplified fill-out-template model.

--- notes/note.py
+++ notes/note.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python
 import datetime as module_datetime
-import enum
-import functools
-import json as module_json
+import itertools
 import os
 import pathlib
 import sys
 import zoneinfo
-from typing import *
 
-import pydantic
 import typer
-from yaml import safe_dump as dump_yaml, safe_load as load_yaml
+
 from ruamel.yaml import YAML
 
-PACIFIC = zoneinfo.ZoneInfo("America/Los_Angeles")
 
 app = typer.Typer()
 
@@ -24,74 +19,17 @@ if "NOTES_PATH" in os.environ:
 else:
     DATA_PATH = None
 
+if "NOTES_TEMPLATE_PATH" in os.environ:
+    TEMPLATE_PATH = pathlib.Path(os.environ["NOTES_TEMPLATE_PATH"])
+else:
+    TEMPLATE_PATH = pathlib.Path(__file__).parent / "templates"
 
-class TimeUnit(enum.Enum):
-    HOUR = "hour"
-    DAY = "day"
-    BUSINESS_DAY = "business_day"
-    WEEK = "week"
-    MONTH = "month"
+TIMEZONE = zoneinfo.ZoneInfo(os.environ.get("NOTES_TIMEZONE", "America/Los_Angeles"))
 
+TEMPLATES = list(itertools.chain(TEMPLATE_PATH.glob("*.md"), TEMPLATE_PATH.glob("*.txt"), TEMPLATE_PATH.glob("*.yaml")))
 
-class NoteBaseModel(pydantic.BaseModel):
-    notes: Optional[str] = None
+COMMAND_TO_TEMPLATE = {f.stem.replace('-', '_'): f for f in TEMPLATES}
 
-    timestamp: module_datetime.datetime = pydantic.Field(
-        default_factory=functools.partial(module_datetime.datetime.now, PACIFIC)
-    )
-    tags: List[str] = pydantic.Field(default_factory=list)
-
-    def yaml(self):
-        value = self.dict()
-        return dump_yaml(value)
-
-    @classmethod
-    def parse_yaml(cls, serialized: str):
-        value = load_yaml(serialized)
-        return cls(**value)
-
-    def load_path(self, file: pathlib.Path):
-        if not file.exists():
-            raise FileNotFoundError(f"{file} does not exist")
-        serialized = file.read_text()
-        return self.parse_yaml(serialized)
-
-    @property
-    def filename(self):
-        return f"{self.timestamp.isoformat()}-{self.__class__.__name__.lower()}.yaml"
-
-    def save(self, text=None, path: pathlib.Path = DATA_PATH):
-        if path is None:
-            raise ValueError("No path specified")
-        path = path / self.filename
-        if text is None:
-            text = self.yaml()
-        path.write_text(text)
-
-
-class Prediction(NoteBaseModel):
-    task: str
-
-    time_unit: TimeUnit
-    expected_completion: module_datetime.datetime | module_datetime.date
-    std: Optional[float] = None
-    range: Optional[Tuple[float, float]] = None
-
-
-
-class Event(NoteBaseModel):
-    event: str
-
-
-@app.command()
-def predict(data_dir=DATA_PATH):
-    # Note: see https://yaml-multiline.info/
-    prediction = Prediction()
-    breakpoint()
-    1 + 1
-
-
-TEMPLATES_DIR = pathlib.Path(__file__).parent / "templates"
 
 def edit(text: str):
     import sys, tempfile, os
@@ -111,37 +49,87 @@ def edit(text: str):
         return edited_message
 
 
-@app.command()
-def event(data_dir=DATA_PATH):
-    event = Event.construct()
-    template = TEMPLATES_DIR / "event.yaml"
-    yaml_obj = YAML()
-    loaded = yaml_obj.load(template.read_text())
-    loaded["timestamp"] = event.dict()["timestamp"]
-    from io import StringIO
-    s = StringIO()
-    yaml_obj.dump(loaded, s)
-    final = edit(s.getvalue())
+
+def do_note(template: pathlib.Path, data_dir=DATA_PATH):
+    timestamp = module_datetime.datetime.now(TIMEZONE)
+    if template.suffix.lower() == ".yaml":
+        yaml_obj = YAML()
+        loaded = yaml_obj.load(template.read_text())
+        loaded["timestamp"] = timestamp
+        from io import StringIO
+        s = StringIO()
+        yaml_obj.dump(loaded, s)
+        value = s.getvalue()
+    else:
+        value = template.read_text()
+        from jinja2 import Environment, BaseLoader
+        rtemplate = Environment(loader=BaseLoader).from_string(value)
+        value = rtemplate.render(date=module_datetime.datetime.now(TIMEZONE).date().isoformat())
+    final = edit(value)
     if final is not None:
+        path = data_dir / f"{timestamp.year}/{timestamp.isoformat()}-{template.name}"
+        path.write_text(final)
+
         # TODO: validate show another editor window with the errors.
-        event = Event.parse_yaml(final)
-        event.save(final)
-        print(f"Event saved to {event.filename}")
+        print(f"{template.stem.title()} saved to {path}")
     else:
         print("No changes made to template; aborting.")
         sys.exit(1)
 
 
-
-@app.command()
-def foo(bar: int):
-    print("foo", bar)
+record = typer.Typer()
+app.add_typer(record, name="record")
 
 
-@app.command()
-def bar(bar: str, baz: bool=False):
-    if baz:
-        print("foo", bar, baz)
+# TODO: how to define a command for each template in the directory without method each time?
+@record.command()
+def due_date(data_dir: pathlib.Path=DATA_PATH):
+    return do_note(COMMAND_TO_TEMPLATE["due_date"], data_dir)
+
+
+@record.command()
+def event(data_dir: pathlib.Path=DATA_PATH):
+    return do_note(COMMAND_TO_TEMPLATE["event"], data_dir)
+
+
+@record.command()
+def metric(data_dir: pathlib.Path=DATA_PATH):
+    return do_note(COMMAND_TO_TEMPLATE["metric"], data_dir)
+
+
+@record.command()
+def note(data_dir: pathlib.Path=DATA_PATH):
+    return do_note(COMMAND_TO_TEMPLATE["note"], data_dir)
+
+
+@record.command()
+def prediction(data_dir: pathlib.Path=DATA_PATH):
+    return do_note(COMMAND_TO_TEMPLATE["prediction"], data_dir)
+
+
+@record.command()
+def task(data_dir: pathlib.Path=DATA_PATH):
+    return do_note(COMMAND_TO_TEMPLATE["task"], data_dir)
+
+
+query = typer.Typer()
+app.add_typer(query, name="query")
+
+@query.command()
+def tasks(data_dir: pathlib.Path=DATA_PATH, show_all: bool=False):
+    """
+    Show all tasks from task and due dates.
+
+    If --show-all is selected, then expired or completed tasks will be included.
+    """
+    yaml = YAML(typ="safe")
+    for task in data_dir.glob("**/*.yaml"):
+        if task.stem.endswith("task") or task.stem.endswith("due_date"):
+            value = yaml.load(task)
+            breakpoint()
+            # if show_all or not task.stem.endswith("_completed"):
+            #     print(task.read_text())
+    raise NotImplementedError()
 
 
 if __name__ == '__main__':
