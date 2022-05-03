3dc7e5c8e24c2ed3a751fd61af48cc48cd53f974
Lucas Wiman <lucas.wiman@akasa.com>
2022-05-03 00:32:03 -0700

    Add failing test for date format.

---
+++ tests/test_parsing.py
@@ -0,0 +1,6 @@
+import datetime
+from notes.parser import parse_datetime_or_delta
+
+def test_parse_datetime_or_delta():
+    assert parse_datetime_or_delta('next thursday', datetime.date(2022, 5, 3)) == datetime.date(2022, 5, 5)
+    assert parse_datetime_or_delta('thursday', datetime.date(2022, 5, 3)) == datetime.date(2022, 5, 12)
